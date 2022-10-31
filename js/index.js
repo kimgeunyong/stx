018ba0077d999b89165776a531de2c7068ef06b9
kimgeunyong <109326337+kimgeunyong@users.noreply.github.com>
2022-10-31 11:31:46 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$("document").ready(function(){
+    // let i = (this).index
+    $(".header .header-list .list-wrap .nav ul li").mouseover(function(){
+        $(".header .side-nav").toggleClass("on")
+    })
+})
